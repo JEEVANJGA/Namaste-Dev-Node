ef6b27ce5a9461e2b721286e8377165aa0d20c21
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2024-08-24 16:07:53 +0530

    feat: Add logging examples in app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// Example 1: Logging a string
+console.log("Hello, world!");
+
+// Example 2: Logging a number
+console.log(42);
+
+// Example 3: Logging an array
+console.log([1, 2, 3]);
+
+// Example 4: Logging an object
+console.log({ name: "John", age: 30 });
+
+// Example 5: Logging a boolean value
+console.log(true);
